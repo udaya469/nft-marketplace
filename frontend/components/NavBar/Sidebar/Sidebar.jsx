c1d8479bcf3cfe8a5a96533916e48fa1c3abe8ba
dark7rey <dark7rey@gmail.com>
2025-01-17 07:12:26 +0000

    Completed till 1:25

---
+++ frontend/components/NavBar/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Internal import 
+import Style from './Sidebar.module.css'
+
+const Sidebar = () =>{
+    return(
+        <div>
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+export default Sidebar;
