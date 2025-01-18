271f374cb8cdf9934e6f8426a67ae63ec940dec0
dark7rey <dark7rey@gmail.com>
2025-01-18 07:29:43 +0000

    Completed till 1:31 / img added

--- frontend/components/NavBar/NavBar.jsx
+++ frontend/components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from 'next/image';
+import Link from "next/link";
+//---- IMPORT ICON 
+import {MdNotificationsNone} from 'react-icons/md';
+import {BsSearch} from 'react-icons/bs';
+import { CgMenuLeft, CgMenuRight} from 'react-icons/cg';
 
 //Internal import 
 import Style from './NavBar.module.css'
-
+import { Discover, HelpCenter, Notification, Profile, Sidebar } from "./index";
+import { Button } from "../compomnentsindex";
 const NavBar = () =>{
     return(
         <div>
