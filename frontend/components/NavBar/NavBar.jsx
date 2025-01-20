e06cbdcd45759700eb21135905421647f1334206
dark7rey <dark7rey@gmail.com>
2025-01-20 08:25:35 +0000

    Completed till 1:46

--- frontend/components/NavBar/NavBar.jsx
+++ frontend/components/NavBar/NavBar.jsx
@@ -10,11 +10,109 @@ import { CgMenuLeft, CgMenuRight} from 'react-icons/cg';
 import Style from './NavBar.module.css'
 import { Discover, HelpCenter, Notification, Profile, Sidebar } from "./index";
 import { Button } from "../compomnentsindex";
-const NavBar = () =>{
-    return(
-        <div>
-            <h1>NavBar</h1>
+import images from "../../img";
+
+const NavBar = () => {
+  //----USESTATE
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e)=> {
+    const btnText = e.target.innerText;
+    if(btnText === 'Discover'){
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }else if(btnText === 'Help Center'){
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else{
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  const openNotification = () => {
+    if(!notification){
+      setNotification(true);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    }else{
+      setNotification(false);
+    }
+  };
+  
+  return (
+    <div className={Style.navbar}>
+        <div className={Style.navbar_container}>
+            <div className={Style.navbar_container_left}> 
+                <div className={Style.logo}>
+                    <Image 
+                    src={images.logo}
+                    alt="NFT MARKET PLACE"
+                    width={100}
+                    height={100}
+                    />
+                </div>
+                <div className={Style.navbar_container_left_box_input}>
+                    <div className={Style.navbar_container_left_box_input_box}>
+                        <input type='text' placeholder="Search NFTs" />
+                        <BsSearch onClick={() => {}} className={Style.search_icon} />
+                    </div>
+                </div>
+            </div>
+          
+            {/*  //END OF LEFT SECTION */} 
+            <div className={Style.navbar_container_right}>
+                <div className={Style.navbar_container_right_discover}>
+                    {/* DISCOVER MENU  */}
+                    <p onClick={(e) => openMenu(e)}>Discover</p>
+                    {discover && (
+                        <div className={Style.navbar_container_right_discover_box}>
+                            <Discover />
+                        </div>
+                    )}  
+                </div>
+                {/* HELP CENTER MENU */}
+                <div className={Style.navbar_container_right_help}>
+                    <p onClick={(e) => openMenu(e)}>Help Center</p>
+                    {help && (
+                        <div className={Style.navbar_container_right_help_box}>
+                            <HelpCenter />
+                        </div>
+                    )}
+                </div>
+                {/* NOTIFICATION */}
+                <div className={Style.navbar_container_right_notify}>
+                    <MdNotifications 
+                    className={Style.notify} 
+                    onClick={() => openNotification()} 
+                    />
+                    {notification &&  <Notification />}
+                </div>  
+                {/* CREATE BUTTON SECTION */}
+                <div className={Style.navbar_container_right_button}>
+                    <Button btnText="Create" />
+                </div>
+                {/* USER PROFILE */}
+                <div className={Style.navbar_container_right_profile_box}>
+                    <div className={Style.navbar_container_right_profile}>
+                     <Image />
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    </div>  
+  );
+};
+
 export default NavBar;
